8896c9b4b916de0ee3dd9b52e624afd6a102057d
Taureon <45183108+Taureon@users.noreply.github.com>
2022-05-25 22:23:57 +0200

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -6,12 +6,10 @@ module.exports = {
         .setName('about')
         .setDescription(`About the bot`),
     async execute(interaction) {
-        const currentTimestamp = Math.round(+new Date() / 1000);
-        const timestampColour = currentTimestamp.toString(16).substring(2, 10);
         const embed = new MessageEmbed()
             .setTitle('ProgressCount95')
             .setDescription(`Version 0.1.0\n\nBot created by <@284804878604435476>`)
-            .setColor(`${timestampColour}`);
+            .setColor(Math.round(+new Date() / 1000).toString(16).substring(2, 10));
         await interaction.reply({embeds: [embed]});
     },
 };
